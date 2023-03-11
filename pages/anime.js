c362acce1120c4a618c1bc3ae58852a6ccabf641
Micah Stotsky <Leowyatt09@gmail.com>
2023-03-11 00:52:09 -0600

    fuck yes

--- pages/anime.js
+++ pages/anime.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import AnimeCard from '../components/AnimeCard';
+import { useAuth } from '../utils/context/authContext';
 import { getAnime } from '../utils/data/animeData';
 
 function Anime() {
   const [anime, setAnime] = useState([]);
+  const { user, updateUser } = useAuth();
 
   useEffect(() => {
     getAnime().then((data) => setAnime(data));
@@ -12,7 +14,7 @@ function Anime() {
   return (
     <div className="d-flex flex-wrap">
       {anime?.map((show) => (
-        <AnimeCard key={show.id} animeObj={show} onUpdate={getAnime} />
+        <AnimeCard user={user} updateUser={updateUser} key={show.id} animeObj={show} onUpdate={getAnime} />
       ))}
     </div>
   );
--- utils/data/watchlistData.js
+++ utils/data/watchlistData.js
@@ -1,69 +1,69 @@
-import { clientCredentials } from '../client';
+// import { clientCredentials } from '../client';
 
-const getWatchlist = () => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/watchlist`)
-    .then((response) => response.json())
-    .then(resolve)
-    .catch(reject);
-});
+// const getWatchlist = () => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/watchlist`)
+//     .then((response) => response.json())
+//     .then(resolve)
+//     .catch(reject);
+// });
 
-const createWatchlist = (watchlist) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/watchlist`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(watchlist),
-  })
-    .then((response) => resolve(response))
-    .catch(reject);
-});
+// const createWatchlist = (watchlist) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/watchlist`, {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/json',
+//     },
+//     body: JSON.stringify(watchlist),
+//   })
+//     .then((response) => resolve(response))
+//     .catch(reject);
+// });
 
-const updateWatchlist = (watchlist, id) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/watchlist/${id}`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(watchlist),
-  })
-    .then((response) => resolve(response))
-    .catch(reject);
-});
+// const updateWatchlist = (watchlist, id) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/watchlist/${id}`, {
+//     method: 'POST',
+//     headers: {
+//       'content-type': 'application/json',
+//     },
+//     body: JSON.stringify(watchlist),
+//   })
+//     .then((response) => resolve(response))
+//     .catch(reject);
+// });
 
-const deleteWatchlist = (watchlist) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/watchlist/${watchlist}`, {
-    method: 'DELETE',
-  })
-    .then(resolve)
-    .catch(reject);
-});
+// const deleteWatchlist = (watchlist) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/watchlist/${watchlist}`, {
+//     method: 'DELETE',
+//   })
+//     .then(resolve)
+//     .catch(reject);
+// });
 
-const watchShow = (id, anime) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/watchlist/${id}/watch`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(anime),
-  })
-    .then((response) => resolve(response))
-    .catch((error) => reject(error));
-});
+// const watchShow = (id, anime) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/watchlist/${id}/watch`, {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(anime),
+//   })
+//     .then((response) => resolve(response))
+//     .catch((error) => reject(error));
+// });
 
-const dropShow = (id, animeId) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/watchlist/${id}/drop`, {
-    method: 'DELETE',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ animeId }),
-  })
-    .then((response) => resolve(response))
-    .catch((error) => reject(error));
-});
+// const dropShow = (id, animeId) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/watchlist/${id}/drop`, {
+//     method: 'DELETE',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify({ animeId }),
+//   })
+//     .then((response) => resolve(response))
+//     .catch((error) => reject(error));
+// });
 
-export {
-  getWatchlist,
-  createWatchlist,
-  updateWatchlist,
-  deleteWatchlist,
-  watchShow,
-  dropShow,
-};
+// export {
+//   getWatchlist,
+//   createWatchlist,
+//   updateWatchlist,
+//   deleteWatchlist,
+//   watchShow,
+//   dropShow,
+// };
